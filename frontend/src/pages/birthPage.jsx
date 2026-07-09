5d18388bfe3f06713207a3123272e26f9d326182
sandeep yadav <sandeepyada234abc@gmail.com>
2026-07-09 11:09:12 +0530

    some changes

--- frontend/src/pages/birthPage.jsx
+++ frontend/src/pages/birthPage.jsx
@@ -1,7 +1,17 @@
 import { TypeAnimation } from "react-type-animation";
+import crown from "../assets/crown.png";
+import heart from "../assets/heart img.png";
+import image2 from "../assets/image copy 3.png";
+import image3 from "../assets/image copy 4.png";
+import cake from "../assets/image copy 5.png";
+import image4 from "../assets/image copy 6.png";
+import image5 from "../assets/image copy 7.png";
+import gift from "../assets/image copy 8.png";
+import dil from "../assets/image copy 9.png";
+import image1 from "../assets/image copy.png";
+import image from "../assets/image.png";
 import BullonExplorer from "../components/BulloonExplorer";
 import FloatingHeart from "../components/FloatingHearts";
-
 import "./style.css";
 
 function BirthdayPage() {
@@ -13,19 +23,19 @@ function BirthdayPage() {
       </div>
         <div className='reet-imgs'>
             <div className="reet-mid">
-                <img src="src/assets/image.png" alt="reet" />
-                <img src="src/assets/image copy.png" alt="reet" />
-                <img src="src/assets/image copy 3.png" alt="reet" />
+                <img src={image} alt="reet" />
+                <img src={image1} alt="reet" />
+                <img src={image2} alt="reet" />
                 </div>
                 <div className="reet-mid">
-                <img src="src/assets/image copy 4.png" alt="reet" />
-                <img src="src/assets/image copy 6.png" alt="reet" />
-                <img src="src/assets/image copy 7.png" alt="reet" />
+                <img src={image3} alt="reet" />
+                <img src={image4} alt="reet" />
+                <img src={image5} alt="reet" />
                 </div>
             </div>
         <div className="mid-container">
       <div className="content">
-        <img className="crown" src="src/assets/crown.png" alt="crown" />
+        <img className="crown" src={crown} alt="crown" />
         <h4 className="birth-text">
           Happy Birthday My Dear bestfriend Reet Kaur
         </h4>
@@ -42,17 +52,17 @@ function BirthdayPage() {
         <div className="cakes">
           <img
             className="cake-img"
-            src="src/assets/image copy 5.png"
+            src={cake}
             alt="cake"
           />
           <img
             className="cake-img"
-            src="src/assets/image copy 8.png"
+            src={gift}
             alt="cake"
           />
           <img
             className="cake-img"
-            src="src/assets/image copy 9.png"
+            src={dil}
             alt="cake"
           />
         </div>
@@ -69,7 +79,7 @@ function BirthdayPage() {
         <div>
           <img
             style={{ width: "150px" }}
-            src="src/assets/heart img.png"
+            src={heart}
             alt="love"
           />
         </div>
